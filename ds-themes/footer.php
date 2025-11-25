1cfb253a5adbcb5ba8d83935cdfe6de830a30afd
melinasalihu <salihumelina8@gmail.com>
2025-11-25 17:34:32 +0100

    lesson 3

--- ds-themes/footer.php
+++ ds-themes/footer.php
@@ -1,5 +1,9 @@
   <footer>
+    <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu','depth'=> 1)); ?>
         <p>Digital School</p>
+        <nav class="footer_menu">
+       
+        </nav>
     </footer>
 </body>
 </html>
--- ds-themes/functions.php
+++ ds-themes/functions.php
@@ -7,4 +7,29 @@ function load_script(){
     wp_enqueue_script('dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', false);
 }
 add_action('wp_enqueue_scripts', 'load_script');
+
+function config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu '=> 'Main Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        )
+        );
+
+    $args = array(
+        'height' => 225,
+        "width" => 1920
+    );
+    add_theme_support('custom-header',$args);    
+    add_theme_support('post-thumbnails',);    
+    add_theme_support('custom-logo', array(
+        'width' => 200,
+        'height' => 110,
+        'flex-height' => true,
+        'flex-width' => true
+    ));  
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5', array('commit-list', 'search-form','gallery', 'caption', 'style', 'script')); 
+}
+add_action('after_setup_theme','config',0);
 ?>
