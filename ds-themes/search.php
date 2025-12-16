98a5e09ca3749e5436bc22ece5c1727f2d3c2d32
melinasalihu <salihumelina8@gmail.com>
2025-12-16 17:37:12 +0100

    commit

---
+++ ds-themes/search.php
@@ -0,0 +1,20 @@
+<?php get_headers(); ?>
+
+<div id="primary">
+    <div id="main">
+     <div class="container">
+
+     <h1>Search results for: <?php echo get_search_query(); ?></h1>
+
+     <?php 
+     get_search_form();
+
+     while ( have_posts() ) : 
+        the_post();
+         get_template_part( 'parts/content', 'search' );
+         endwhile;
+     ?>
+     </div>
+    </div>
+</div>
+<?php get_footer(); ?>
